be7f7387e2ea669d68bc664fcf969c9ce596df1a
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2023-05-21 21:12:00 +0300

    add enter username display

---
+++ app/src/main/java/com/example/mywhatsapp/SetUsernameActivity.java
@@ -0,0 +1,14 @@
+package com.example.mywhatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetUsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_username);
+    }
+}
